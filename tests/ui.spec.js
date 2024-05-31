f27382a00bf639ea8b09119ec5a2089849eb37f4
Dominick Jay <dominickjay217@gmail.com>
2024-05-31 15:06:07 +0100

    remove screenshot test

--- tests/ui.spec.js
+++ tests/ui.spec.js
@@ -1,10 +1,9 @@
-const { test, expect } = require('@playwright/test');
+// const { test, expect } = require('@playwright/test');
 
-test('example test', async ({ page }) => {
-    // THIS TEST WILL FAIL THE FIRST TIME AS THERE'S NO COMPARISON IMAGE
-    await page.goto('/')
-    await page.waitForTimeout(5000)
+// test('example test', async ({ page }) => {
+//     // THIS TEST WILL FAIL THE FIRST TIME AS THERE'S NO COMPARISON IMAGE
+//     await page.goto('/')
+//     await page.waitForTimeout(5000)
 
-    await expect(page).toHaveScreenshot({ fullPage: true, animations: 'disabled', timeout: 120000, maxDiffPixels: 100 });
-
-});
+//     await expect(page).toHaveScreenshot({ fullPage: true, animations: 'disabled', timeout: 120000, maxDiffPixels: 100 });
+// });
